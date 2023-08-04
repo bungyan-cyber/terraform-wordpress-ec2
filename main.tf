3862bde39919344130a0d59dc2d79607d023cf9b
Dian Pratama <dianpratama07@gmail.com>
2023-08-04 18:52:36 +0700

    add terraform wordpress

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-southeast-1"  # Change to your desired region
+}
+
+resource "aws_instance" "wordpress_instance" {
+  ami           = "ami-002843b0a9e09324a"  # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-027e97b03510547df"  # Replace with your subnet ID
+  key_name      = "bungyan"  # Replace with your key pair name
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "wordpress-instance"
+  }
+
+  # Security Group Configuration
+  vpc_security_group_ids = ["sg-0574f8cc70ca0126d"]  # Replace with your security group ID
+}
+
